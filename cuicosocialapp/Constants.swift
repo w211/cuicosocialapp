1b1accfb940c01be683d3d590eff538155c40e38
Richard Cuico <marshallrichardcuico@Richards-iMac.local>
2016-05-30 16:16:12 -0700

    Formatted Login Screen

---
+++ cuicosocialapp/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  cuicosocialapp
+//
+//  Created by Richard Cuico on 5/30/16.
+//  Copyright © 2016 Richard Cuico. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// We won't put a class on this so that everything here will be globally accessible throughout the files
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+    // If you want to get the propers color value you have to take the value of it and you have to divide it by 255
+    // Because 0.0 is black and 1.0 is white so 157.0 / 255.0 is in between 0 and 1
+    // Now go back to MaterialVIew and set the color of the shadow
